a5de49aad0b5f901fca1043a1582a7a2f24dece6
Emmanuel Correa <ebcorreac@gmail.com>
2023-12-14 01:15:54 -0300

    [FIX]Host

--- js/config/connection.js
+++ js/config/connection.js
@@ -1,7 +1,7 @@
 const mode = true
 
-const api = mode == false ? 'http://127.0.0.1:3001/api/' : 'https://simple-parking-api.onrender.com/api/';
-const url = origin === "http://127.0.0.1:5500" || origin.includes('http://192.168.1.') ? "" : "/";
+const api = mode == false ? 'http://localhost:3000/api/' : 'https://simple-parking-api.onrender.com/api/';
+const url = origin === "http://localhost:5500" || origin.includes('http://192.168.1.') ? "" : "/";
 
 // function urlAdaptive() {
 //     const urls = document.querySelectorAll('#url');
